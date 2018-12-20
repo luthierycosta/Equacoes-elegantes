d732b80b8d7a983bc5fa9d48f18801fa2607d95e
luthierycosta <luthierycosta2010@gmail.com>
2018-12-20 00:10:14 -0200

    Simplificando linhas

--- sistemasEquacao.scala
+++ sistemasEquacao.scala
@@ -1,52 +1,46 @@
 import scala.collection.mutable.Map
 import scala.math._
 
-abstract class Equation {
+class Equation(initial: Double) {
+	// deixá-las como membros da classe foi uma forma de deixá-las como variáveis "globais"
 	val a = 0.9259;	 val b1 = 0.15;		val b2 = 1.5;
 	val g = 0.1;	 val rl = 0.002;	val bp = 1;
 	val m1 = -1.4357;val m0 = -0.7879;	val h = 0.02;
 
-    val valsArray = Map.empty[Int,Double]
-    def apply(n: Int): Double
+    val valsArray = Map(0 -> initial)
 }
 
-object x extends Equation {
+object x extends Equation(0.2) {
 	def apply(n: Int): Double = {
 		if (!valsArray.contains(n))
-			valsArray += (n -> {
-				if (n == 0) 0.2
-				else if (n == 1) x(n+1)
+			valsArray(n) =
+				if (n == 1) x(n+1)
 				else x(n-2) + h * a * (y(n-2) - x(n-2)) * pow(b1,-1) - f(n-2) * pow(b1,-1)
-			})
 		return valsArray(n)
 	}
 }
 
-object y extends Equation {
+object y extends Equation(0.1) {
 	def apply(n: Int): Double = {
 		if (!valsArray.contains(n))
-			valsArray += (n -> {
-				if (n == 0) 0.1
-				else if (n == 1) y(n+1)
+			valsArray(n) =
+				if (n == 1) y(n+1)
 				else y(n-2) + h * a * (y(n-2) - x(n-2)) * pow(b2,-1) + z(n-2) * pow(b2,-1)
-			})
 		return valsArray(n)
 	}
 }
 
-object z extends Equation {
+object z extends Equation(0.1) {
 	def apply(n: Int): Double = {
 		if (!valsArray.contains(n))
-			valsArray += (n -> {
-				if (n == 0) 0.1
-				else if (n == 1) z(n+1)
+			valsArray(n) =
+				if (n == 1) z(n+1)
 				else z(n-2) + h * (y(n-2) - rl*z(n-2)) * pow(g, -1)
-			})
 		return valsArray(n)
 	}
 }
 
-object f extends Equation {
+object f extends Equation(0) {
 	def apply(x: Int) = m0 * x + 0.5 * (m1-m0) * (abs(x+bp) - abs(x-bp))
 }
 
